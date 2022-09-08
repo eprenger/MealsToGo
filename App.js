93bbc74958fb1c4824f9f40476b0e54532f6954c
Evan Prenger <eprenger23@gmail.com>
2022-09-07 22:39:47 -0400

    basic layout test

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+          style={{
+            padding: 16,
+            backgroundColor: "green",
+          }}
+        >
+          <Text>search</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "blue",
+            flex: 1,
+            padding: 16,
+            flexGrow: 1,
+          }}
+        >
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style="auto" />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
