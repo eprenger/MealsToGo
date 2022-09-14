0bfea7317d7562404ebd335710f2cae93ed88a7f
Evan Prenger <eprenger23@gmail.com>
2022-09-13 21:52:12 -0400

    fix status bar for android

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
+const isAndroid = Platform.OS === "android";
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
         <View
           style={{
             padding: 16,
@@ -24,7 +32,7 @@ export default function App() {
           <Text>list</Text>
         </View>
       </SafeAreaView>
-      <StatusBar style="auto" />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
